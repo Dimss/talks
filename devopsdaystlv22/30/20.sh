fa02214d0008ee470548b55b72e245c4788afb1c
Dmitry Kartsev <dmitry.kartsev@cnvrg.io>
2022-12-05 17:26:20 +0200

    wip on 40

--- devopsdaystlv22/30/20.sh
+++ devopsdaystlv22/30/20.sh
@@ -1,5 +1,9 @@
-docker run \
- --gpus=0  \
- -v $(pwd)/gpu-test.py:/tmp/gpu-test.py \
- tensorflow/tensorflow:latest-gpu \
- python /tmp/gpu-test.py
+for c in gpu-1 gpu-2; do
+  docker run \
+   --gpus=0  \
+   --name $c \
+   -d \
+   -v $(pwd)/gpu-test.py:/tmp/gpu-test.py \
+   tensorflow/tensorflow:latest-gpu \
+   bash -c "sleep inf"
+done
