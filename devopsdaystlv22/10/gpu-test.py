918fe3f22b048391200becba6c07548c96dd04f2
Dmitry Kartsev <dmitry.kartsev@cnvrg.io>
2022-12-06 19:52:28 +0200

    fix

--- devopsdaystlv22/10/gpu-test.py
+++ devopsdaystlv22/10/gpu-test.py
@@ -3,5 +3,5 @@ import time
 from datetime import datetime as dt
 while True:
    print(f"[{dt.now()}] {tf.config.list_physical_devices('GPU')}", flush=True)
-   print(tf.reduce_sum(tf.random.normal([1000, 1000])))
+   tf.reduce_sum(tf.random.normal([1000, 1000]))
    time.sleep(1)
