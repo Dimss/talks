626ce62fb20775b3eefd4f2b4787fad2584cae05
Dmitry Kartsev <dmitry.kartsev@cnvrg.io>
2022-12-06 13:55:06 +0200

    updates for 50

--- devopsdaystlv22/60/cleanup.sh
+++ devopsdaystlv22/60/cleanup.sh
@@ -1,4 +1,4 @@
 curl --header "Content-Type: application/json-patch+json" \
 --request PATCH \
---data '[{"op": "remove", "path": "/status/capacity/cnvrg.io~100metagpu"}]' \
+--data '[{"op": "remove", "path": "/status/capacity/cnvrg.io~1metagpu"}]' \
 http://localhost:8001/api/v1/nodes/devopsdaystlv01/status
